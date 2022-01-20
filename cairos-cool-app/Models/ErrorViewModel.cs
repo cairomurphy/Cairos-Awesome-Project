7f0487bc874e2b29e90e635a224fb8c3971631fb
cairomurphy <84107491+cairomurphy@users.noreply.github.com>
2022-01-19 17:36:26 -0800

    initial commit

---
+++ cairos-cool-app/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace cairos_cool_app.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
